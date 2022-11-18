7a901c5c8c80dc5c8c4aacd62686c1680780dabb
Laura Salazar <lau_sa99@outlook.com>
2022-11-17 19:47:38 -0500

    Initial commit

---
+++ app/src/main/java/com/taller2/tutoapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.taller2.tutoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button ButtonInicioSesion;
+    Button ButtonCrearCuenta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButtonInicioSesion = findViewById(R.id.buttonIniciar);
+        ButtonCrearCuenta = findViewById(R.id.buttonCrearCuenta);
+
+
+        ButtonCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this,SelectOptionUser.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/taller2/tutoapp/SelectOptionUser.java
@@ -0,0 +1,13 @@
+package com.taller2.tutoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectOptionUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option_user);
+    }
+}
