25a249777f59bfdeb49b467b4fd65116ea600c98
Laura Salazar <lau_sa99@outlook.com>
2022-11-24 02:24:21 -0500

    Se arregló el registro y se conectaron las ventanas faltantes en el proceso

--- app/src/main/java/com/taller2/tutoapp/TutoriaUnoActivity.java
+++ app/src/main/java/com/taller2/tutoapp/TutoriaUnoActivity.java
@@ -5,23 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
+public class TutoriaUnoActivity extends AppCompatActivity {
 
-public class InicioActivity extends AppCompatActivity {
-
-    LinearLayout ButtonTutoriaUno;
-
+    LinearLayout ButtonMensajeTUno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_tutoria_uno);
 
-        ButtonTutoriaUno = findViewById(R.id.button_materia_t1);
+        ButtonMensajeTUno = findViewById(R.id.button_mensaje_t1);
 
-        ButtonTutoriaUno.setOnClickListener(new View.OnClickListener() {
+        ButtonMensajeTUno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToSelectAuth();
@@ -29,7 +26,7 @@ public class InicioActivity extends AppCompatActivity {
         });
     }
     private void goToSelectAuth() {
-        Intent intent = new Intent(InicioActivity.this,TutoriaUnoActivity.class);
+        Intent intent = new Intent(TutoriaUnoActivity.this,TutoriaUnoMensajeActivity.class);
         startActivity(intent);
     }
 }
